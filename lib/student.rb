35366bbbff5581c4f30f6a2d6f7e08584481ff3d
Anu Khambete <anukhambete@gmail.com>
2018-06-14 18:01:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,13 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
 
+    DB[:conn].execute(sql,grade)
 
   end
 
